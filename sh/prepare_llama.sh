45b562efb652dc6fcf5b798513b86c6026c28ae4
yshr-926 <hiromari0926@gmail.com>
2023-09-21 20:40:39 +0900

    fix sh

--- sh/prepare_llama.sh
+++ sh/prepare_llama.sh
@@ -1,19 +1,15 @@
-models=('Llama-2-7b-hf' 'Llama-2-13b-hf' 'Llama-2-70b-hf')
+models=('Llama-2-7b-hf')
 datasets=('dolly' 'lima')
 
-for dataset in ${datasets[@]}
+for model in ${models[@]}
 do
-    for model in ${models[@]}
+    python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/meta-llama/$model &&
+    for dataset in ${datasets[@]}
     do
-        python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/meta-llama/$model &&
         if [ $dataset = 'dolly' ]; then
-            if [ ! -d data/$dataset-$model ]; then
-            python scripts/prepare_$dataset.py --checkpoint_dir checkpoints/meta-llama/$model --destination_path data/$dataset-$model 
-            fi
+            python scripts/prepare_$dataset.py --checkpoint_dir checkpoints/meta-llama/$model --destination_path data/$dataset-$model --max_seq_length 2048
         else
-            if [ ! -d data/$dataset-$model ]; then
-            python scripts/prepare_$dataset.py --checkpoint_dir checkpoints/meta-llama/$model --destination_path data/$dataset-$model --access_token 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
-            fi
+            python scripts/prepare_$dataset.py --checkpoint_dir checkpoints/meta-llama/$model --destination_path data/$dataset-$model --access_token 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 --max_seq_length 2048
         fi
     done
 done
