19bb825e9c3f809d2607522658c890c6bddfbd45
yshr-926 <hiromari0926@gmail.com>
2023-09-28 21:35:52 +0900

    fix sh

--- sh/prepare_llama.sh
+++ sh/prepare_llama.sh
@@ -1,8 +1,9 @@
-models=('Llama-2-7b-hf')
+models=('Llama-2-13b-hf')
 datasets=('dolly' 'lima')
 
 for model in ${models[@]}
 do
+    python scripts/download.py --repo_id meta-llama/$model --access_token "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" &&
     python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/meta-llama/$model &&
     for dataset in ${datasets[@]}
     do
