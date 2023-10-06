919dcff0e94423e6128252f4ec192d381f728418
sakaguchi <ikuzim018@gmail.com>
2023-10-06 12:20:14 +0900

    必要なデータセットのみを使えるようにしました

--- scripts/prepare_flan.py
+++ scripts/prepare_flan.py
@@ -16,10 +16,79 @@ sys.path.append(str(wd))
 from lit_gpt.tokenizer import Tokenizer
 
 """
-python3 scripts/prepare_flan.py \
+python3 script/prepare_flan.py \
     --checkpoint_dir "checkpoints/EleutherAI/pythia-70m" \
-    --destination_path data/pythia/pythia-70m/flan
+    --destination_path data/pythia/pythia-70m/flan-test
 """
+
+DATASET = {
+    'aeslc_10templates',
+    'ag_news_subset_10templates',
+    'anli_r1_10templates',
+    'anli_r2_10templates',
+    'anli_r3_10templates',
+    'arc_challenge_10templates',
+    'arc_easy_10templates',
+    'bool_q_10templates',
+    'cb_10templates',
+    'cnn_dailymail_10templates',
+    'cola_10templates',
+    # 'common_gen_10templates',
+    'copa_10templates',
+    'coqa_10templates',
+    'cosmos_qa_10templates',
+    # 'dart_10templates',
+    'definite_pronoun_resolution_10templates',
+    'drop_10templates',
+    # 'e2e_nlg_10templates',
+    'fix_punct_10templates',
+    'gigaword_10templates',
+    # 'glue_mrpc_10templates',
+    # 'glue_qqp_10templates',
+    'hellaswag_10templates',
+    # 'imdb_reviews_10templates',
+    'math_dataset_10templates',
+    'mnli_matched_10templates',
+    'mnli_mismatched_10templates',
+    'multi_news_10templates',
+    'multirc_10templates',
+    'natural_questions_10templates',
+    'openbookqa_10templates',
+    'opinion_abstracts_idebate_10templates',
+    'opinion_abstracts_rotten_tomatoes_10templates',
+    # 'para_crawl_enes_10templates',
+    # 'paws_wiki_10templates',
+    'piqa_10templates',
+    'qnli_10templates',
+    'quac_10templates',
+    'record_10templates',
+    'rte_10templates',
+    'samsum_10templates',
+    # 'sentiment140_10templates',
+    'snli_10templates',
+    'squad_v1_10templates',
+    'squad_v2_10templates',
+    # 'sst2_10templates',
+    'story_cloze_10templates',
+    # 'stsb_10templates',
+    'trec_10templates',
+    'trivia_qa_10templates',
+    'true_case_10templates',
+    # 'web_nlg_en_10templates',
+    'wic_10templates',
+    'wiki_lingua_english_en_10templates',
+    'wmt14_enfr_10templates',
+    # 'wmt16_translate_csen_10templates',
+    # 'wmt16_translate_deen_10templates',
+    # 'wmt16_translate_fien_10templates',
+    # 'wmt16_translate_roen_10templates',
+    # 'wmt16_translate_ruen_10templates',
+    # 'wmt16_translate_tren_10templates',
+    'wnli_10templates',
+    'word_segment_10templates',
+    'wsc_10templates',
+    # 'yelp_polarity_reviews_10templates'
+}
 
 def prepare(
     destination_path: Path = Path("data/flan"),
@@ -30,6 +99,7 @@ def prepare(
     include_multiturn_conversations: bool = False,
     data_repo_id: str = "Muennighoff/flan",
     ignore_index: int = -1,
+    ignore_dataset: bool = True,
     access_token: Optional[str] = os.getenv("HF_TOKEN"),
     max_seq_length: Optional[int] = None,
 ) -> None:
@@ -50,14 +120,14 @@ def prepare(
     from datasets import load_dataset
 
     dataset = load_dataset(data_repo_id, use_auth_token=access_token)
-    train_data = format_dataset(dataset["train"], include_multiturn_conversations)
+    train_data = format_dataset(dataset["train"], include_multiturn_conversations, ignore_dataset)
 
     # test set is present but doesn't have any solutions, so we cannot use it here
     # but have to create our own
     # for consistency with prepare_alpaca.py and prepare_dolly.py
     
     # lima ではコメントアウトされていたが, flan では test データにも教師ラベルがあるからいけそう
-    test_set = format_dataset(dataset["test"], include_multiturn_conversations)
+    test_set = format_dataset(dataset["test"], include_multiturn_conversations, ignore_dataset)
 
     print("Loading tokenizer...")
     tokenizer = Tokenizer(checkpoint_dir)
@@ -97,18 +167,22 @@ def prepare(
     ]
     torch.save(test_set, destination_path / "test.pt")
 
-def format_dataset(dataset_partition, include_multi_turn_conversations):
+def format_dataset(dataset_partition, include_multi_turn_conversations, ignore_dataset):
     formatted_ds = []
     
     for entry in dataset_partition:
         convo = entry["inputs"]
         target = entry["targets"]
+        task = entry["task"]
+        if ignore_dataset and task not in DATASET:
+            continue
         if include_multi_turn_conversations:
             for i in range(0, len(convo) - 1, 2):
                 # 会話形式のところは変更していません(include_multi_turn_conversations = false (default) で使う)
                 formatted_ds.append({"instruction": convo[i], "input": "", "output": convo[i + 1]})
         else:
             formatted_ds.append({"instruction": convo, "input": "", "output": target})
+        sys.exit()
 
     return formatted_ds
 
