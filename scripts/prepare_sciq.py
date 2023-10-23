b292c76252dc094c52637cb486d79dd9fb1c8329
sakaguchi <ikuzim018@gmail.com>
2023-10-23 18:18:28 +0900

    fix prepare_sciq.py

--- scripts/prepare_sciq.py
+++ scripts/prepare_sciq.py
@@ -20,7 +20,7 @@ from lit_gpt.tokenizer import Tokenizer
 python3 script/prepare_sciq.py \
     --checkpoint_dir "checkpoints/meta-llama/Llama-2-7b-hf" \
     --destination_path data/sciq-Llama-2-7b-hf \
-    --max_seq_length 2048
+    # --max_seq_length 2048
 """
 
 def prepare(
@@ -115,7 +115,7 @@ def format_dataset(dataset_partition, include_multi_turn_conversations):
         else:
             formatted_ds.append({"instruction": entry["question"], "input": ', '.join(choices), "output": entry["correct_answer"]})
         
-        return formatted_ds
+    return formatted_ds
 
 
 def prepare_sample(example: dict, tokenizer: Tokenizer, max_length: int, mask_inputs: bool, ignore_index: int):
